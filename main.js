4ba78dd1600ac073bae3f7fd47bcaf778dc3c1db
Josephsan-2011 <83101904+Josephsan-2011@users.noreply.github.com>
2022-03-11 19:53:23 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,27 @@ function preload() {
 function setup() {
     canvas=createCanvas(300,300)
     canvas.center()
+    video = createCapture(VIDEO)
+    video.size(300,300)
+    video.hide()
+    poseNet = ml5.poseNet(video,JosephS)
+    poseNet.on('pose',allpoint)
+}
+function allpoint(result) {
+    if (result.length>0) {
+        
+    
+    console.log(result)
+    console.log("nose x="+result[0].pose.nose.x)
+    console.log("nose y="+result[0].pose.nose.y)
+    }
 }
 function draw() {
-    
+    image(video,0,0,300,300)
 }
 function take_snapshot() {
     save("filter.png")
+}
+function JosephS() {
+    console.log("model-loaded")
 }
