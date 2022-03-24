97d8453f3bb7474fae5488f87bb08b422690088e
Josephsan-2011 <83101904+Josephsan-2011@users.noreply.github.com>
2022-03-23 20:47:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+noseX=0
+noseY=0
+leftEyeX=0
+leftEyeY=0
+rightEye=0
 function preload() {
-    
+    hat=loadImage("https://i.postimg.cc/6pgvm0ZC/1486189.png")
+    beard=loadImage("https://i.postimg.cc/nhGPpXHR/Nice-Png-beard-png-59256.png")
 }
 function setup() {
     canvas=createCanvas(300,300)
@@ -17,10 +23,20 @@ function allpoint(result) {
     console.log(result)
     console.log("nose x="+result[0].pose.nose.x)
     console.log("nose y="+result[0].pose.nose.y)
+    noseX=result[0].pose.nose.x
+    noseY=result[0].pose.nose.y
+    leftEyeX=result[0].pose.leftEye.x
+    leftEyeY=result[0].pose.leftEye.y
+
     }
 }
 function draw() {
     image(video,0,0,300,300)
+    fill("blue")
+    stroke("red")
+    //circle(leftEyeX,leftEyeY,20)
+    image(hat,leftEyeX-80,leftEyeY-150,150,70)
+    image(beard,noseX-40,noseY-40,80,130)
 }
 function take_snapshot() {
     save("filter.png")
